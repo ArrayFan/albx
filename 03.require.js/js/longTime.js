76605ed2ca3788280e288541e00b838a447e9f43
whf <2329986624@qq.com>
2018-06-06 19:40:39 +0800

    2018-6-6 19:40 晚间提交

---
+++ 03.require.js/js/longTime.js
@@ -0,0 +1,11 @@
+// 十分耗费性能的js
+function fbnq(n){
+    // n==1 n==2 返回1
+    if(n==1||n==2){
+        return 1;
+    }
+    // 其他情况 返回 n-1 +n-2
+    return fbnq(n-1)+fbnq(n-2);
+}
+var result = fbnq(30);
+console.log(result);
